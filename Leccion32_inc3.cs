ec9c20651b2da446761dbfa3c5af1c511e945cce
22ACM01-AlexCalderon <a.calderon.6ddb@gmail.com>
2026-07-21 22:56:04 -0600

    Add triangle type classification program

---
+++ Leccion32_inc3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese la medida del primer lado del triángulo:");
+        double lado1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la medida del segundo lado:");
+        double lado2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la medida del tercer lado:");
+        double lado3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n--- Resultado ---");
+
+        
+        if (lado1 == lado2 && lado2 == lado3)
+        {
+            Console.WriteLine("El triángulo es EQUILÁTERO (todos sus lados son iguales).");
+        }
+        
+        else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3)
+        {
+            Console.WriteLine("El triángulo es ESCALENO (todos sus lados son diferentes).");
+        }
+       
+        else
+        {
+            Console.WriteLine("El triángulo es ISÓSCELES (tiene dos lados iguales).");
+        }
+    }
+}
